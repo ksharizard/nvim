06a7d97f3f35b43388f4f4866f192a964d25285a
GhoulBoii <code.ksh@erine.email>
2025-01-30 01:14:08 +0530

    feat: remove notify for now

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,47 +1,47 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-      },
-      signature = {
-        enabled = false,
-      },
-      hover = {
-        enabled = false,
-      },
-    },
-    -- you can enable a preset for easier configuration
-    presets = {
-      bottom_search = true,
-      command_palette = true,
-      long_message_to_split = true,
-      inc_rename = false,
-      lsp_doc_border = false,
-    },
-    routes = {
-      {
-        filter = {
-          event = "msg_show",
-          kind = "",
-          find = "written",
-        }
-      },
-      {
-        filter = {
-          event = "msg_show",
-          kind = "",
-          find = "change",
-        },
-      },
-    }
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
-  }
+--   "folke/noice.nvim",
+--   event = "VeryLazy",
+--   opts = {
+--     lsp = {
+--       override = {
+--         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--         ["vim.lsp.util.stylize_markdown"] = true,
+--         ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+--       },
+--       signature = {
+--         enabled = false,
+--       },
+--       hover = {
+--         enabled = false,
+--       },
+--     },
+--     -- you can enable a preset for easier configuration
+--     presets = {
+--       bottom_search = true,
+--       command_palette = true,
+--       long_message_to_split = true,
+--       inc_rename = false,
+--       lsp_doc_border = false,
+--     },
+--     routes = {
+--       {
+--         filter = {
+--           event = "msg_show",
+--           kind = "",
+--           find = "written",
+--         }
+--       },
+--       {
+--         filter = {
+--           event = "msg_show",
+--           kind = "",
+--           find = "change",
+--         },
+--       },
+--     }
+--   },
+--   dependencies = {
+--     "MunifTanjim/nui.nvim",
+--     "rcarriga/nvim-notify",
+--   }
 }
