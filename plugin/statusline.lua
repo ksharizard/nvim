0fdcef2636161dfae11b9029f792f803b78d0d99
ksharizard <code@kshdev.slmail.me>
2026-04-08 16:36:17 +0530

    feat(statusline): switch to mini.statusline

--- plugin/statusline.lua
+++ plugin/statusline.lua
@@ -1,4 +1,4 @@
-local function getWords()
+local function get_words()
   local filetype = vim.bo.filetype
   if filetype == "text" or filetype == "" or filetype == "markdown" then
     local wordcount = vim.fn.wordcount()
@@ -14,30 +14,77 @@ local function getWords()
   return ""
 end
 
-vim.pack.add({ "https://github.com/nvim-lualine/lualine.nvim" })
-require("lualine").setup({
-  theme = "dracula",
-  options = {
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = { 'filetype', getWords },
-    lualine_z = { 'progress' },
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
-})
+vim.pack.add({ "https://github.com/nvim-mini/mini.statusline" })
+require("mini.statusline").setup({
+  content = {
+    active = function()
+      -- Mode
+      local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+      mode = string.upper(mode)
 
+      -- Filename
+      local filename = vim.fn.expand("%:t")
+      if filename == "" then
+        filename = "[No Name]"
+      end
+
+      -- Git branch, and Git Diff
+      local git = MiniStatusline.section_git({ icon = "", trunc_width = 75 })
+      if git ~= "" then
+        git = string.match(git, "^%S+%s+%S+") or git
+      end
+
+      local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+
+      local file_and_git = filename
+      if git ~= "" then
+        file_and_git = file_and_git .. " | " .. git
+      end
+      if diff ~= "" then
+        file_and_git = file_and_git .. " " .. diff
+      end
+
+      -- Search Selection Count
+      local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+      -- Word Count
+      local word_count = get_words()
+
+      -- Filetype and Icon
+      local filetype = vim.bo.filetype
+      local icon = ""
+      local has_devicons, devicons = pcall(require, "nvim-web-devicons")
+      if has_devicons then
+        icon = devicons.get_icon(filename, vim.fn.expand("%:e"), { default = true }) or ""
+      end
+
+      local file_info = ""
+      if filetype ~= "" then
+        if icon ~= "" then
+          file_info = icon .. " " .. filetype
+        else
+          file_info = filetype
+        end
+      end
+
+      -- Location (Percentage only)
+      local location = "%2p%%"
+
+      -- Combine the groups
+      return MiniStatusline.combine_groups({
+        -- LEFT SIDE
+        { hl = mode_hl, strings = { mode } },
+        { hl = "MiniStatuslineDevinfo", strings = { file_and_git } },
+
+        "%<", -- Mark general truncate point
+        "%=", -- Right align everything after this point
+
+        -- RIGHT SIDE
+        { hl = "MiniStatuslineDevinfo", strings = { search } },
+        { hl = "MiniStatuslineDevinfo", strings = { word_count } },
+        { hl = "MiniStatuslineFileinfo", strings = { file_info } },
+        { hl = mode_hl, strings = { location } },
+      })
+    end,
+  },
+})
