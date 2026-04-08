c5990f30965ff22ec528f1f80b8b96cbfed2ead6
ksharizard <code@kshdev.slmail.me>
2026-04-08 16:50:59 +0530

    style(plugins): fix formatting

--- plugin/python.lua
+++ plugin/python.lua
@@ -1,23 +1,22 @@
-vim.pack.add({"https://github.com/mfussenegger/nvim-dap"})
-vim.pack.add({"https://github.com/nvim-neotest/nvim-nio"})
-vim.pack.add({"https://github.com/rcarriga/nvim-dap-ui"})
+vim.pack.add({ "https://github.com/mfussenegger/nvim-dap" })
+vim.pack.add({ "https://github.com/nvim-neotest/nvim-nio" })
+vim.pack.add({ "https://github.com/rcarriga/nvim-dap-ui" })
 require("dapui").setup({
   config = function()
     local listener = require("dap").listeners
     listener.after.event_initialized["dapui_config"] = function() require("dapui").open() end
   end,
 })
-vim.pack.add({"https://github.com/mfussenegger/nvim-dap-python"})
+vim.pack.add({ "https://github.com/mfussenegger/nvim-dap-python" })
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'python',
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
   callback = function()
     -- uses the debugypy installation by mason
     -- local debugpyPythonPath = require("mason-registry").get_package("debugpy"):get_install_path()
     --   .. "/venv/bin/python3"
     -- require("dap-python").setup(debugpyPythonPath, {})
     require("dap-python").setup("python3")
-  end
+  end,
 })
 -- vim.pack.add({"https://github.com/chrisgrieser/nvim-puppeteer"})
-
