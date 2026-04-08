abef11e1ca7c0de7a0c56c61c1816d5da5206c1d
ksharizard <code@kshdev.slmail.me>
2026-04-08 18:46:05 +0530

    refactor(ftplugin): vimscript to lua

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,5 +1,5 @@
 -- use pep8 standards
-vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
 
 -- folds based on indentation https://neovim.io/doc/user/fold.html#fold-indent
 -- if you are a heavy user of folds, consider using `nvim-ufo`
