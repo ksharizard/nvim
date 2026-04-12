99374a20ae97fc35a2951e65b6b4570d4145181e
ksharizard <code@kshdev.slmail.me>
2026-04-12 16:36:08 +0530

    feat(lsp): move python and lua lsp to lsp folder

--- after/lsp/basedpyright.lua
+++ after/lsp/basedpyright.lua
@@ -34,16 +34,19 @@ return {
     'Pipfile',
     '.git',
   },
-  ---@type lspconfig.settings.basedpyright
   settings = {
     basedpyright = {
+      disableOrganizeImports = true,
       analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
         autoSearchPaths = true,
         diagnosticMode = 'openFilesOnly',
         -- https://docs.basedpyright.com/latest/configuration/language-server-settings/
         -- Explicitly setting `basedpyright.analysis.useLibraryCodeForTypes` is **discouraged** by the official docs.
         -- Because it will override per-project configurations like `pyproject.toml`.
         -- If left unset, its default value is `true`, and it can be correctly overridden by project config files.
+      -- Using Ruff's import organizer
       },
     },
   },
---
+++ after/lsp/lua_ls.lua
@@ -0,0 +1,37 @@
+return {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".luarc.json", ".luarc.jsonc" },
+  telemetry = { enabled = false },
+  formatters = {
+    ignoreComments = false,
+  },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+
+        path = {
+          "lua/?.lua",
+          "lua/?/init.lua",
+        },
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          -- '${3rd}/luv/library',
+          -- '${3rd}/busted/library',
+        },
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = vim.api.nvim_get_runtime_file('', true),
+      },
+      signatureHelp = { enabled = true },
+    },
+  },
+}
